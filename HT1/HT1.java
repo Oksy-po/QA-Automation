7c65cbdd9c20608d4a39837433f826daf07c656a
Oksy-po <120282970+Oksy-po@users.noreply.github.com>
2022-12-21 02:10:17 +0300

    HT1

---
+++ HT1/HT1.java
@@ -0,0 +1,42 @@
+public class HT1 {
+    public static void main(String[] args) {
+        // Task 1
+        byte myProgramme = 2;
+        System.out.println("myProgramme = " + myProgramme);
+        byte coolest = 4;
+        System.out.println("coolest = " + coolest);
+        short myHolidays = 777;
+        System.out.println("myHolidays = " + myHolidays);
+        short iNeedHolidays = 888;
+        System.out.println("iNeedHolidays = " + iNeedHolidays);
+        int itIsnoteasy = 777777777;
+        System.out.println("itIsnoteasy = " + itIsnoteasy);
+        int soonHappyNewYear = 888888888;
+        System.out.println("soonHappyNewYear = " + soonHappyNewYear);
+        long iLikegettingGifts = 843927402740127012L;
+        System.out.println("iLikegettingGifts = " + iLikegettingGifts);
+        long iLikeCakes = 87492649812491270L;
+        System.out.println("iLikeCakes = " + iLikeCakes);
+
+        // Task2
+        float x = 7.77f;
+        System.out.println("x = " + x);
+        float xx = 2.456f;
+        System.out.println("xx = " + xx);
+        double y = 33.75;
+        System.out.println("y = " + y);
+        double yy = 345.3;
+        System.out.println("yy = " + yy);
+        boolean isTrue = true;
+        System.out.println("isTrue = " + isTrue);
+        boolean isFalse = false;
+        System.out.println("isFalse = " + isFalse);
+        char triangle = '\u25B4';
+        System.out.println("triangle = " + triangle);
+        char smile = '\u1F60';
+        System.out.println("smile = " + smile);
+        char ring = '\u1F48';
+        System.out.println("ring = " + ring);
+    }
+}
+
