35c77df58303ba1f49c1a61e3503f8d3b4adb132
Oksy-po <120282970+Oksy-po@users.noreply.github.com>
2022-12-23 22:27:23 +0300

    Lesson 3

---
+++ HT1/src/lesson_03/PracticeTask.java
@@ -0,0 +1,27 @@
+package src.lesson_03;
+
+import java.util.Scanner;
+
+public class PracticeTask {
+    public static void main(String[] args) {
+
+        String name;
+
+        int age;
+
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter your name: ");
+          name = input.nextLine();
+        System.out.println("Please enter your age: ");
+
+          age = input.nextInt();
+        System.out.println(" Please enter your grade: ");
+        double grade = input.nextDouble();
+        System.out.println("name = " + name);
+        System.out.println("age = " + age);
+        System.out.println("grade = " + grade);
+        System.out.println("Finished");
+
+    }
+}
