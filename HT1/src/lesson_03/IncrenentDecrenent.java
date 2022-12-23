5e1bb951854025a92c1d90388d1879347d3e230a
Oksy-po <120282970+Oksy-po@users.noreply.github.com>
2022-12-23 23:51:21 +0300

    IncrenentDecrenent

---
+++ HT1/src/lesson_03/IncrenentDecrenent.java
@@ -0,0 +1,19 @@
+package src.lesson_03;
+
+public class IncrenentDecrenent {
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println("a = " + a);
+        a = a + 6;
+        System.out.println("a = " + a);
+
+        a += 10;
+        System.out.println("a = " + a);
+        a += 3;
+        System.out.println("a = " + a);
+        a++;
+        System.out.println("a = " + a);
+
+
+    }
+}
