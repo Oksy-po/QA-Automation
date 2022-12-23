58e134a03500d3a08be92596499d7baf09290e1e
Oksy-po <120282970+Oksy-po@users.noreply.github.com>
2022-12-24 00:38:27 +0300

    ComparisonOperators

---
+++ HT1/src/lesson_03/ComparisonOperators.java
@@ -0,0 +1,19 @@
+package src.lesson_03;
+
+public class ComparisonOperators {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int c = 6;
+
+        System.out.println("a == b " + (a == b));
+        System.out.println("c == b " + (c == b));
+        System.out.println("a <= b " + (a <= b));
+        System.out.println("c <= b " + (c <= b));
+        System.out.println("b != a " + (b != a));
+        System.out.println("!true = " + !true);
+        System.out.println("!false = " + !false);
+
+
+    }
+}
